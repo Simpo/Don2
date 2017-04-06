03fbea3b578afed63dba4004d305bb6f26f26b7b
Simpo <marcusgathii2011@gmail.com>
2017-04-06 20:54:59 +0300

    Familialize with git

---
+++ js/jscode.js
@@ -0,0 +1,6 @@
+function changeStyle(){
+
+  /*var text = document.getElementById("paral").style.color = "blue";*/
+  /*text = document.getElementById("paral").style.backgroundColor = "yellow";*/
+  text = document.getElementById("paral").style.fontStyle = "italic";
+}
